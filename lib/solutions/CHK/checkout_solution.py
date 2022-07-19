ae56f044485a82611b8a80a7eb022a35a863af2a
sbx_user1051 <sbx_user1051@169.254.151.173>
2022-07-19 23:07:25 +0000

    Tue Jul 19 23:07:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,12 +1,19 @@
 
 from collections import Counter
 from dataclasses import dataclass
-from typing import Iterable
+from typing import Dict, Iterable
 
 @dataclass
 class Item:
     name: str
     price: int
+
+
+@dataclass
+class MultiPrice:
+    items: Dict[str, int] 
+    price: int
+
 
 ITEMS = {
     "A": Item(name="A", price=50),
@@ -14,6 +21,11 @@ ITEMS = {
     "C": Item(name="C", price=20),
     "D": Item(name="D", price=15)
 }
+
+MULTIPRICES = [
+    MultiPrice(items={"A":3}, price=130),
+    MultiPrice(items={"B":2}, price=45),
+]
 
 def is_valid_skus(skus: str, valid_skus: Iterable[str]) -> bool:
     return all(sku in valid_skus for sku in skus)
@@ -27,5 +39,6 @@ def checkout(skus: str) -> int:
     sku_counter = Counter(skus)
 
     print(sku_counter)
+
 
 
