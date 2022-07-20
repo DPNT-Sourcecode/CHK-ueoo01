3c03d181111b172756efc0bf59e9637acb52fc28
sbx_user1051 <sbx_user1051@169.254.151.173>
2022-07-20 00:23:28 +0000

    Wed Jul 20 00:23:28 UTC 2022

--- lib/solutions/CHK/supermarket.py
+++ lib/solutions/CHK/supermarket.py
@@ -1,25 +1,27 @@
-
+import string
 
 from lib.solutions.CHK.models import Freebie, Item, MultiPrice
 
+prices = [50, 30, 20, 15, 40, 10 ,20, 10, 35, 60, 80, 90, 15, 40, 10, 50, 30, 50, 30, 20, 40, 50, 20, 90, 10, 50]
 
 ITEM_CATALOG = {
-    "A": Item(name="A", price=50),
-    "B": Item(name="B", price=30),
-    "C": Item(name="C", price=20),
-    "D": Item(name="D", price=15),
-    "E": Item(name="E", price=40),
-    "F": Item(name="F", price=10),
+    sku: Item(name=sku, price=price) for sku, price in zip(string.string.ascii_uppercase, prices)
 }
 
 # order in multiprice group defines precedence
 MULTIPRICE_GROUPS = [
     [MultiPrice(items={"A": 5}, price=200), MultiPrice(items={"A": 3}, price=130)],
     [MultiPrice(items={"B": 2}, price=45)],
+    [MultiPrice(items={"H": 10}, price=80), MultiPrice(items={"H": 5}, price=45)],
+    [MultiPrice(items={"K": 2}, price=150)],
+    [MultiPrice(items={"P": 5}, price=200)],
+    [MultiPrice(items={"Q": 3}, price=80)],
+    [MultiPrice(items={"V": 3}, price=130), MultiPrice(items={"V": 2}, price=90)],
 ]
 
 FREEBIES = [
     Freebie(items={"E": 2, "B": 1}, freebies={"B": 1}),
     Freebie(items={"F": 3}, freebies={"F": 1})
 ]
+
 
