9140bd8940a058695e5c326fcbf0fde0f0bcbc6f
sbx_user1051 <sbx_user1051@169.254.151.173>
2022-07-20 00:41:45 +0000

    Wed Jul 20 00:41:45 UTC 2022

--- lib/solutions/CHK/supermarket.py
+++ lib/solutions/CHK/supermarket.py
@@ -1,4 +1,7 @@
+from functools import partial
 import string
+from typing import Dict, Iterable
+from lib.solutions.CHK.models import DynamicMultiPrice
 
 from solutions.CHK.models import Freebie, Item, MultiPrice
 
@@ -7,6 +10,17 @@ prices = [50, 30, 20, 15, 40, 10 ,20, 10, 35, 60, 70, 90, 15, 40, 10, 50, 30, 50
 ITEM_CATALOG = {
     sku: Item(name=sku, price=price) for sku, price in zip(string.ascii_uppercase, prices)
 }
+
+def any_n_of_items(n: int, skuset: Iterable[str], sku_counter: Dict[str, int]) -> Dict[str, int]:
+    total, matched = 0, {}
+    for sku in skuset:
+        if sku in sku_counter:
+            matched[sku] = sku_counter[sku] if total + sku_counter[sku] <= n else n - total
+    
+        if total >= n:
+            return matched
+    return {}
+            
 
 # order in multiprice group defines precedence
 MULTIPRICE_GROUPS = [
@@ -17,6 +31,12 @@ MULTIPRICE_GROUPS = [
     [MultiPrice(items={"P": 5}, price=200)],
     [MultiPrice(items={"Q": 3}, price=80)],
     [MultiPrice(items={"V": 3}, price=130), MultiPrice(items={"V": 2}, price=90)],
+    [
+        DynamicMultiPrice(
+            items=partial(any_n_of_items, 3, ("S", "T", "X", "Y", "Z")),
+            price=45
+        )
+    ]
 ]
 
 FREEBIES = [
@@ -26,5 +46,6 @@ FREEBIES = [
     Freebie(items={"R": 3, "Q": 1}, freebies={"Q": 1}),
     Freebie(items={"U": 4}, freebies={"U": 1}),
 ]
+
 
 
